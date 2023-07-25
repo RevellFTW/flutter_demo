cf9c685d05be1043d210a5442e26f5473fb5991a
Bertalan Krisztian <krisztian652@gmail.com>
2023-07-25 19:03:30 +0200

    ios settings and vs debugging

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,6 +3,20 @@ import FlutterMacOS
 
 @NSApplicationMain
 class AppDelegate: FlutterAppDelegate {
+   if #available(iOS 10.0, *) {
+       // For iOS 10 display notification (sent via APNS)
+       UNUserNotificationCenter.current().delegate = self
+       let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+       UNUserNotificationCenter.current().requestAuthorization(
+         options: authOptions,
+         completionHandler: { _, _ in }
+       )
+     } else {
+       let settings: UIUserNotificationSettings =
+         UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+       application.registerUserNotificationSettings(settings)
+     }
+     application.registerForRemoteNotifications()
   override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
     return true
   }
